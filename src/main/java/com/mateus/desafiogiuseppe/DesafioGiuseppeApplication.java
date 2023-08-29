7a4a4de94c959c2c820a6927cb391c009ec850a9
Mateus Pereira <mateus.maia@dbserver.com.br>
2023-08-29 17:20:13 -0300

    build: config projeto

---
+++ src/main/java/com/mateus/desafiogiuseppe/DesafioGiuseppeApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.desafiogiuseppe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGiuseppeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGiuseppeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateus/desafiogiuseppe/DesafioGiuseppeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.desafiogiuseppe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioGiuseppeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
